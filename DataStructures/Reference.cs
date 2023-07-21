046244f43c3416ccb4956b3aee926d3f09d49275
Marie Korandová <mariekorandova@gmail.com>
2023-07-21 20:03:37 +0200

    Adding B:<book number> as a reference to just a book

--- DataStructures/Reference.cs
+++ DataStructures/Reference.cs
@@ -30,9 +30,9 @@
         //B:<book number> <chapter number>:<verse number #1>,<verse number #2>[<more comma-separated verse numbers>]
         //B:<book number> <chapter number>
         //B:<book number> <chapter number start>-<chapter number end>
-
+        //B:<book number>
         */
-        static public List<Reference> ToTarget(this string unparsed)
+        static public List<Reference> ToTarget(this string unparsed) //input is a string reference without "B:"
         {
             int l = unparsed.Length;
             char[] delimeters = new char[] { ' ', ':', '-', ',' };
@@ -58,42 +58,49 @@
             int count = targetNumbers.Count;
             int delCount = targetDelimeters.Count;
 
-            if (count < 2) { throw new FormatException(); }
+            if (count == 0) { throw new FormatException(); }
 
             int book = targetNumbers[0];
+            if (count == 1)         //B:<book number
+            {
+                targets.Add(new Reference(book, 0));
+                return targets;
+            }
+            
             int chapterStart = targetNumbers[1];
 
-            if (count == 2)
+            if (count == 2) //B:<book number> <chapter number>
             {
                 targets.Add(new Reference(book, chapterStart));
                 return targets;
             }
             if (delCount == 0) { throw new FormatException(); }
-            if (count == 3 && targetDelimeters[0] == '-')
+        
+            if (count == 3 && targetDelimeters[0] == '-') //B:<book number> <chapter number start>-<chapter number end>
             {
                 targets.Add(new Reference(book, chapterStart, default, targetNumbers[2], default));
                 return targets;
             }
             int verseStart = targetNumbers[2];
 
-            if (count == 3)
+            if (count == 3) //B:<book number> <chapter number>:<verse number>
             {
                 targets.Add(new Reference(book, chapterStart, verseStart));
                 return targets;
             }
             bool verseAndSpan = (targetDelimeters[0] == ':' && targetDelimeters[1] == '-');
-            if (count == 4 && delCount == 2 && verseAndSpan)
+            if (count == 4 && delCount == 2 && verseAndSpan) //B:<book number> <chapter number>:<verse number>-<end verse number>
             {
                 targets.Add(new Reference(book, chapterStart, verseStart, default, targetNumbers[3]));
                 return targets;
             }
-            if (count == 5 && delCount == 3 && verseAndSpan)
+            if (count == 5 && delCount == 3 && verseAndSpan) //B:<book number> <chapter number>:<verse number>-<end chapter number>:<end verse number>
             {
                 targets.Add(new Reference(book, chapterStart, verseStart, targetNumbers[3], targetNumbers[4]));
                 return targets;
             }
             bool stackedVerse = targetDelimeters.Skip(1).All(d => d == ',');
-            if (stackedVerse && count - 2 == delCount)
+            if (stackedVerse && count - 2 == delCount) //B:<book number> <chapter number>:<verse number #1>,<verse number #2>[<more comma-separated verse numbers>]
             {
                 for (int i = 2; i < count; i++)
                 {
