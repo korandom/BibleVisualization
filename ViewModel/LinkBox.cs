7c8f51aba78c5f023355ae046a71b2f745dbeec4
Marie Korandová <mariekorandova@gmail.com>
2023-09-02 02:16:50 +0200

    Starting point for ModelView

---
+++ ViewModel/LinkBox.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataStructures;
+using FindReferencesForRequirements;
+
+namespace ViewModel
+{
+    internal class LinkBox 
+    {
+        bool source = true;
+        VerseEnumerator verseEnumerator;
+
+        public LinkBox( VerseEnumerator verseEnumerator)
+        {
+            this.verseEnumerator = verseEnumerator;
+        }
+    }
+}
---
+++ ViewModel/RequirementBox.cs
@@ -0,0 +1,44 @@
+﻿using FindLinksForRequirements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataStructures;
+using FindReferencesForRequirements;
+
+namespace ViewModel
+{
+    internal class RequirementBox
+    {
+        public string Text = "";
+        public string RequirementDescription = "";
+        InputParser ip = new InputParser();
+        BibleText bible;
+
+        public RequirementBox(BibleText bible)
+        {
+            this.bible = bible;
+        }
+
+        public void LoadProperties(string requirement)
+        {
+            try
+            {
+                List<Reference> requirementReferences = ip.Parse(requirement);
+                StringBuilder text = new StringBuilder();
+                foreach (Reference reference in requirementReferences)
+                {
+                    VerseEnumerator vs = bible.GetEnumerator(reference);
+                    while(vs.MoveNext()) 
+                    {
+                        text.Append(vs.Current());
+                    } 
+                }
+                RequirementDescription = requirement;
+                Text = text.ToString();
+            }
+            catch (FormatException e) { Text = e.Message; }
+        }
+    }
+}
